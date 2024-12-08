7a32f2e439fa926a3dde60fb395733a666b972a3
piraye-bulut <anikpirayebulut@hotmail.com>
2024-12-09 00:47:26 +0300

    ProcFile

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
-    origin: 'http://localhost:3000', // Frontend URL
+    origin: [
+      'http://localhost:3000', // Geliştirme ortamı için
+      'https://peakfileapp.netlify.app/' // Üretim ortamı için
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  await app.listen(3001); // Backend dinleme portu
+  await app.listen(process.env.PORT ||3001); // Backend dinleme portu
 }
 bootstrap();
